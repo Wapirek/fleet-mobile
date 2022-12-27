646fc72dc72a3e9d5ed3d713d2ef29729b6759f6
Jedrelek <jedrelek@gmail.com>
2022-12-27 02:08:47 +0100

    add progress bar to goals

---
+++ lib/pages/goal_add_page.dart
@@ -0,0 +1,60 @@
+import 'package:fleet_mobile/widgets/amount_field_widget.dart';
+import 'package:fleet_mobile/widgets/button_widget.dart';
+import 'package:fleet_mobile/widgets/isperiodic_filed_widget.dart';
+import 'package:fleet_mobile/widgets/name_field_widget.dart';
+import 'package:flutter/material.dart';
+
+class IncomeAddPage extends StatefulWidget {
+  const IncomeAddPage({Key? key}) : super(key: key);
+
+  @override
+  State<IncomeAddPage> createState() => _IncomeAddPageState();
+}
+
+class _IncomeAddPageState extends State<IncomeAddPage> {
+  final formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final amountController = TextEditingController();
+  final periodicController = TextEditingController();
+  @override
+  void dispose() {
+    nameController.dispose();
+    amountController.dispose();
+    periodicController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dodaj przychód")),
+
+      body: Padding(
+        padding: EdgeInsets.symmetric( vertical: 16.0, horizontal: 8.0),
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: AutofillGroup(
+              child: Column(
+                children: [
+                  NameFieldWidget(controller: nameController),
+                  const SizedBox(height: 8),
+                  AmountFieldWidget(controller: amountController),
+                  const SizedBox(height: 8),
+                  IsPeriodicFieldWidget(controller: periodicController,),
+                  buildButton()
+                ],
+              )
+            )
+          )
+
+        ),
+      ),
+
+    );
+  }
+  Widget buildButton() => ButtonWidget(
+    text: 'Zapisz',
+    onClicked: (){},
+  );
+}
